5eb25a12f9bc9bdeaa7c4a98eb8cf88fd6246334
morphew-ai <m0rph3w41@gmail.com>
2025-09-04 12:34:24 -0300

    Update C6_pix.js

--- C6_pix.js
+++ C6_pix.js
@@ -1,1 +1,41 @@
+var regex = /R\$ (.+,.+), para (.*?), em (\d{2}\/\d{2}\/\d{4})/;
+var match = texto.match(regex);
+var amount = match[1].replace('.','').replace(',','.');
+var payee = match[2];
+var date = match[3];
 
+// Converte data e hora para ISO 8601
+var splittedDate = date.split('/');
+var isoDate = `${splittedDate[2]}-${splittedDate[1]}-${splittedDate[0]}T$00:00:00`;
+
+var airtable_data = {
+  records: [
+    {
+      fields: {
+        Type: 'PIX',
+        Amount: amount,
+        Payee: payee,
+        Date: isoDate,
+        Latitude: gl_latitude,
+        Longitude: gl_longitude,
+        MapUrl: gl_map_url 
+      }
+    }
+  ]
+}
+
+var airtable_json = JSON.stringify(airtable_data, null, 2);
+
+var title = payee;
+var notes = `Destinatario: ${payee}\nValor: ${amount}\nData: ${date}\nMapa: ${gl_map_url}`;
+
+var cashew_data = {
+        amount: '-' + amount,
+        title: title,
+        category: 'Indefinido',
+        date: isoDate,
+        notes: notes
+    };
+
+
+var params = new URLSearchParams(cashew_data).toString();
